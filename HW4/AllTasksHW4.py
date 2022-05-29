412aecf1c3f6ad365aa2c3caf96293d44e1a9877
DimS <dimsed13@yandex.ru>
2022-05-30 00:17:53 +0300

    Решена 3 задача д/з 4 семинара

--- HW4/AllTasksHW4.py
+++ HW4/AllTasksHW4.py
@@ -32,36 +32,56 @@
 # последовательность. Пример: [1, 5, 2, 3, 4, 6, 1, 7] => [1, 2, 3] или [1, 7] или [1, 6, 7] и т.д.
 # Порядок элементов менять нельзя
 
-from random import randint
+# from random import randint
 
 
-list_in = [1, 5, 2, 3, 4, 6, 1, 7]
-list_out = []
-val_max = 0
+# list_in = [1, 5, 2, 3, 4, 6, 1, 7]
+# list_out = []
+# val_max = 0
 
-for val in list_in:
-    if(val > val_max):
-        val_max = val
-        list_out.append(val)
-print(list_out)
+# for val in list_in:
+#     if(val > val_max):
+#         val_max = val
+#         list_out.append(val)
+# print(list_out)
 
 
 # 2. Создать и заполнить файл случайными целыми значениями. Выполнить сортировку содержимого
 # файла по возрастанию.
 
-path = 'file4_2.txt'
+# path = 'file4_2.txt'
 
-max_val = 100
-quantity = 10
-data = open(path, 'w')
-for i in range(quantity):
-    data.write(f'{str(randint(0, max_val))},')
-data.close()
+# max_val = 100
+# quantity = 10
+# data = open(path, 'w')
+# for i in range(quantity):
+#     data.write(f'{str(randint(0, max_val))},')
+# data.close()
+
+# with open(path, 'r') as fr:
+#     list_int = fr.readline().split(',')
+#     list_int.pop()
+#     list_int.sort(key=(int))
+# print(list_int)
+
+
+# 3. Вот вам файл с тысячей чисел. https://cloud.mail.ru/public/DQgN/LqoQzPEec
+# Задача: найти триплеты и просто выводить их на экран. Триплетом называются три числа,
+# которые в сумме дают 0. (решение будет долгим, ибо является демонстрационным при теме
+# многопоточного программирования).
+
+path = '1Kints.txt'
 
 with open(path, 'r') as fr:
-    list_int = fr.readline().split(',')
-    list_int.pop()
-    list_int.sort(key=(int))
-print(list_int)
-
-
+    inp_list = fr.readlines()
+    inp_list.pop()
+    list_val = list(map(int, inp_list))
+count = 0
+for vali in list_val:
+    for valj in list_val:
+        for valk in list_val:
+            if(vali + valj + valk == 0):
+                print(f'{vali} + {valj} + {valk} = 0')
+                count += 1
+print(f'Количество триплетов равно {count}') # Количество триплетов равно 420
+# print(list_val)
